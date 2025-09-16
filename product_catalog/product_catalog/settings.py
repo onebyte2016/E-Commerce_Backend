a2230684ef73beb70d62c42069393901015f5747
onebyte2016 <onebyte2016@gmail.com>
2025-09-16 11:17:43 +0000

    optimize database queries with indexing

--- product_catalog/product_catalog/settings.py
+++ product_catalog/product_catalog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1hlox@*30ay6v7+%3%vn)r^mg)o=!x0b293a-6q-*2py-v!-nw'
+# SECRET_KEY = 'django-insecure-1hlox@*30ay6v7+%3%vn)r^mg)o=!x0b293a-6q-*2py-v!-nw'
+
+
+
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "fallback_key")
+DEBUG = os.getenv("DJANGO_DEBUG", "true").lower() == "true"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -136,7 +142,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
