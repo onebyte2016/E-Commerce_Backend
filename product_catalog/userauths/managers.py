bd1e5829c3549524d791e5ecd018e80c70cdaf7d
onebyte2016 <onebyte2016@gmail.com>
2025-09-26 18:38:55 +0000

    updated customuser manager to allow login from frontend

--- product_catalog/userauths/managers.py
+++ product_catalog/userauths/managers.py
@@ -1,20 +1,50 @@
-from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.base_user import BaseUserManager
+
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(self, email, username=None, password=None, **extra_fields):
         if not email:
             raise ValueError("Email is required")
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+
+        user = self.model(
+            email=email,
+            username=username,   # ✅ include username
+            **extra_fields
+        )
         user.set_password(password)
-        user.save()
+        user.save(using=self._db)
         return user
-    
-    def create_superuser(self, email, password=None, **extra_fields):
+
+    def create_superuser(self, email, username=None, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
         if not extra_fields.get("is_staff"):
-            raise ValueError("Superuser must have is_staff True")
+            raise ValueError("Superuser must have is_staff=True")
         if not extra_fields.get("is_superuser"):
-            raise ValueError("superuser must have is_superuser true")
-        return self.create_user(email, password, **extra_fields)
+            raise ValueError("Superuser must have is_superuser=True")
+
+        return self.create_user(email, username, password, **extra_fields)
+
+
+
+
+
+
+
+# class CustomUserManager(BaseUserManager):
+#     def create_user(self, email, username, password=None, **extra_fields):
+#         if not email:
+#             raise ValueError("Users must have an email address")
+
+#         email = self.normalize_email(email)
+#         user = self.model(email=email, username=username, **extra_fields)
+#         user.set_password(password)   # ✅ hashes the password
+#         user.save(using=self._db)
+#         return user
+
+#     def create_superuser(self, email, username, password=None, **extra_fields):
+#         extra_fields.setdefault("is_staff", True)
+#         extra_fields.setdefault("is_superuser", True)
+
+#         return self.create_user(email, username, password, **extra_fields)
