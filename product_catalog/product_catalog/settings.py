73be024242aefb2f111b57c5fdf290886c371e8d
onebyte2016 <onebyte2016@gmail.com>
2025-09-26 23:50:37 +0000

    updated user models

--- product_catalog/product_catalog/settings.py
+++ product_catalog/product_catalog/settings.py
@@ -186,7 +186,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODELS = 'userauths.CustomUser'
+# AUTH_USER_MODELS = 'userauths.CustomUser'
+AUTH_USER_MODEL = 'userauths.CustomUser'
+
 
 SWAGGER_SETTINGS = {
     "SECURITY_DEFINITIONS": {
--- product_catalog/userauths/models.py
+++ product_catalog/userauths/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from .managers import CustomUserManager
-# Create your models here.
 
-class CustomUser(AbstractBaseUser):
+class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
-    username = models.CharField(unique=True, max_length=100,null=True, blank=True)
+    username = models.CharField(unique=True, max_length=100, null=True, blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
@@ -14,3 +13,24 @@ class CustomUser(AbstractBaseUser):
 
     def __str__(self):
         return self.email
+
+
+
+
+
+# from django.db import models
+# from django.contrib.auth.models import AbstractBaseUser
+# from .managers import CustomUserManager
+# # Create your models here.
+
+# class CustomUser(AbstractBaseUser):
+#     email = models.EmailField(unique=True)
+#     username = models.CharField(unique=True, max_length=100,null=True, blank=True)
+
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = ['username']
+
+#     objects = CustomUserManager()
+
+#     def __str__(self):
+#         return self.email
